c0733cb4bcdd9a7ff1076376ce05333074b3f0f3
mjmcc <mattdev0630@gmail.com>
2017-05-17 00:19:18 -0400

    change

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,21 +1,32 @@
 #pragma warning(disable:4996)
-#include <iostream>
-#include "HidCom.h"
-#include <stdio.h>
+
 #include "DuckyParser.h"
-#include <cstring>
+#include "HidCom.h"
+
+#include <stdio.h>
+#include <fstream>	
+#include <string>
 
 using namespace std;
 
-int main() 
+int main(int argc, char *argv[]) 
 {
-	FILE* file = fopen("test.txt","wb");
+	char* deviceFileName = argv[1];
+	char* codeFileName = argv[2];
+
+	FILE* file = fopen(deviceFileName, "wb");
 
 	HidCommunication hidCom(file);
 	DuckyParser parser(&hidCom);
 	parser.initKeyMap();
 
-	parser.parseProgram(string("PRESS A; PRESS B; RELEASE A"));
+
+	string str;
+	ifstream readFile(codeFileName);
+
+	while (getline(readFile, str)){
+		parser.parseProgram(str);
+	}
 
 	fclose(file);
 }
